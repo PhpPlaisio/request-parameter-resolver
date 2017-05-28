c708f8d2bf236c7c20c1ddd47063cf28ddc787db
Paul Water <p.r.water@setbased.nl>
2017-05-28 23:21:53 +0200

    Initial commit.

---
+++ src/RequestParameterResolver.php
@@ -0,0 +1,26 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\RequestParameterResolver;
+
+//----------------------------------------------------------------------------------------------------------------------
+/**
+ * Interface for RequestParameterResolvers. A RequestParameterResolver resolves the CGI parameters from a clean URL. Or
+ * in other words uncleans a clean URL.
+ */
+interface RequestParameterResolver
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Resolves the CGI parameters of a clean URL and enhances $_GET accordingly.
+   *
+   * @return void
+   *
+   * @api
+   * @since 1.0.0
+   */
+  public function resolveRequestParameters();
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
