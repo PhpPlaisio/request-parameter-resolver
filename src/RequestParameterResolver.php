0ac847d3a39c2a05a0fb351415d49d6eb549bbe5
Paul Water <p.r.water@setbased.nl>
2025-06-27 12:49:11 +0200

    Full redesign of the interface.

--- src/RequestParameterResolver.php
+++ src/RequestParameterResolver.php
@@ -12,10 +12,13 @@ interface RequestParameterResolver
   /**
    * Resolves the CGI parameters of a clean URL and returns all CGI parameters.
    *
+   * @param string $requestUri The requested relative URL. The query part must be ignored.
+   *
+   * @return array
    * @api
    * @since 2.0.0
    */
-  public function resolveRequestParameters(): array;
+  public function resolveRequestParameters(string $requestUri): array;
 
   //--------------------------------------------------------------------------------------------------------------------
 }
