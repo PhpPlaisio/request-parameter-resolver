496d2fc205ebb7bcde067b90ae5daf8a9f868aad
Paul Water <p.r.water@setbased.nl>
2025-06-27 10:19:57 +0200

    Full redesign of the interface.

--- src/RequestParameterResolver.php
+++ src/RequestParameterResolver.php
@@ -4,21 +4,18 @@ declare(strict_types=1);
 namespace Plaisio\RequestParameterResolver;
 
 /**
- * Interface for RequestParameterResolvers. A RequestParameterResolver resolves the CGI parameters from a clean URL. Or
- * in other words uncleans a clean URL.
+ * Interface for RequestParameterResolvers. A RequestParameterResolver resolves the CGI parameters from a clean URL.
  */
 interface RequestParameterResolver
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
-   * Resolves the CGI parameters of a clean URL and enhances $_GET accordingly.
-   *
-   * @return void
+   * Resolves the CGI parameters of a clean URL and returns all CGI parameters.
    *
    * @api
-   * @since 1.0.0
+   * @since 2.0.0
    */
-  public function resolveRequestParameters(): void;
+  public function resolveRequestParameters(): array;
 
   //--------------------------------------------------------------------------------------------------------------------
 }
